011925afcfdd29b9dd938545c359138b5a9519fb
masli747 <93626559+masli747@users.noreply.github.com>
2025-11-24 12:02:41 -0800

    Modified routes.js to use pageRouter.js

--- conductor-server/adapters/in/routes/routes.js
+++ conductor-server/adapters/in/routes/routes.js
@@ -1,33 +1,15 @@
+/**
+ * routes.js is the central index pointing towards other API routers in the server.
+ */
+
 import path from 'path';
 import { makeAuthRouter } from './authRoutes.js';
+import { makePageRouter } from './pageRouter.js';
 
 export function mountRoutes(app, container) {
-  const { authController } = container;
-
-  // root → login
-  app.get('/', (req, res) => res.redirect('/auth/login'));
-
+  // Auth APIs
   app.use('/auth', makeAuthRouter({ authController: container.authController }));
 
-//   // instructor area (HTML pages)
-//   app.get(
-//     '/instructor',
-//     requireAuth('instructor'),
-//     (req, res) => {
-//       res.sendFile(path.join(process.cwd(), 'frontend/instructor/dashboard.html'));
-//     }
-//   );
-
-//   // student area
-//   app.get(
-//     '/student',
-//     requireAuth('student'),
-//     (req, res) => {
-//       res.sendFile(path.join(process.cwd(), 'frontend/student/dashboard.html'));
-//     }
-//   );
-
-//   // Example JSON APIs used *within* those pages
-//   app.get('/api/courses', requireAuth(), container.courseController.list);
-//   app.post('/api/courses', requireAuth('instructor'), container.courseController.create);
+  // HTML pages
+  app.use('/', makePageRouter(container));
 }
