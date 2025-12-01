588106bddd7df6986ed23af034cbc2f0b7d0d322
masli747 <93626559+masli747@users.noreply.github.com>
2025-11-30 23:52:41 -0800

    Linting style fixes

--- conductor-server/adapters/in/routes/cssRouter.js
+++ conductor-server/adapters/in/routes/cssRouter.js
@@ -7,7 +7,7 @@ export function makeCssRouter() {
   /**
    * ===== SHARED CSS =====
    */
-  
+
   router.get('/auth.css', (req, res) => {
     res.sendFile(path.join(process.cwd(), 'frontend/src/css/auth.css'));
   });
@@ -18,11 +18,11 @@ export function makeCssRouter() {
 
   router.get('/student/dashboard.css', (req, res) => {
     res.sendFile(path.join(process.cwd(), 'frontend/src/css/student/dashboard.css'));
-  })
+  });
 
   router.get('/student/courses.css', (req, res) => {
     res.sendFile(path.join(process.cwd(), 'frontend/src/css/student/courses.css'));
-  })
+  });
 
   /**
    * ===== INSTRUCTOR CSS =====
@@ -30,16 +30,16 @@ export function makeCssRouter() {
 
   router.get('/instructor/dashboard.css', (req, res) => {
     res.sendFile(path.join(process.cwd(), 'frontend/src/css/instructor/dashboard.css'));
-  })
+  });
 
   router.get('/instructor/courses.css', (req, res) => {
     res.sendFile(path.join(process.cwd(), 'frontend/src/css/instructor/courses.css'));
-  })
+  });
 
   // FIXME: Can we confirm if this styles.css is required please?
   router.get('/instructor/styles.css', (req, res) => {
     res.sendFile(path.join(process.cwd(), 'frontend/src/css/instructor/styles.css'));
-  })
+  });
 
   return router;
 }
