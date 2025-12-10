f7092f65d246f3ded2e371e63879eb6da8430eb7
Neil <1134421816@qq.com>
2025-12-10 14:53:11 -0800

    wire course title with API for instructor course page

--- frontend/src/js/instructor/courses.js
+++ frontend/src/js/instructor/courses.js
@@ -354,3 +354,39 @@ document.addEventListener('DOMContentLoaded', async () => {
 window.showModal = showModal;
 window.hideModal = hideModal;
 window.hideGroupDetailModal = hideGroupDetailModal;
+
+
+async function getCourseInfoById() {
+    const pathParts = window.location.pathname.split('/');
+    const courseId = pathParts[pathParts.length - 1];
+
+    console.log('Course ID:', courseId);
+
+    try {
+        const res = await fetch(`/api/postman/course?course_id=${courseId}`, {
+            method: "GET",
+            credentials: "include",
+        });
+        if (!res.ok) {
+            const errorData = await res.json();
+            throw new Error(errorData.error || 'Failed to fetch course');
+        }
+
+        const courseData = await res.json();
+        console.log('Course data:', courseData);
+        return courseData;
+
+    } catch (e) {
+        console.error('Error fetching course:', e);
+        // alert('Error fetching course: ' + e.message);
+    }
+}
+
+
+getCourseInfoById()
+    .then(course => {
+        document.getElementById('course-title').textContent = course.name + " " + course.code;
+    })
+    .catch(err => {
+        console.error('Error fetching course:', err);
+    });
